e4bf2df084de41b8ac7b4760b0a6701b15c94dc7
Guillaume Huertas <guillaumehuertas@free.fr>
2020-01-20 21:46:20 +0100

    Ajout Post Jeux Vidéos

--- routes/jeuxVideo.js
+++ routes/jeuxVideo.js
@@ -1,15 +1,54 @@
 let express = require('express');
+let Client = require('node-rest-client').Client;
 
 let router = express.Router();
+let client = new Client();
 
 let titleApp = "Liste Jeux Vidéos :";
 
 router.get('/', (req, res) => {
-   res.render('jeuxVideos', { title: titleApp });
+   client.registerMethod("jsonMethod", "http://localhost:8080/jeuxVideos", "GET");
+
+   client.methods.jsonMethod(function (data, response) {
+
+      console.log(data.content);
+
+      const jeuxvideos = data.content;
+
+      res.render('jeuxVideos', { title: titleApp, jeuxvideos: jeuxvideos});
+   });
 });
 
 router.get('/new', (req, res) => {
    res.render('new-jeux-video', { title: titleApp });
+});
+
+
+
+router.post('/new', (req, res) => {
+
+   var args = {
+      data: {
+         "titre": "Spyro",
+         "developpeur": "Insomniac Games",
+         "editeur": "Activision",
+         "statut": true,
+         "note": 20,
+         "description": "Meilleur remake de jeux de plateforme"
+      },
+      headers: { "Content-Type": "application/json" }
+   };
+   client.registerMethod("postMethod", "http://localhost:8080/consoles/1051/jeuxVideos", "POST");
+
+   client.methods.postMethod(args, function (data, response) {
+
+      // parsed response body as js object
+      // console.log(data);
+      console.log("passe ici");
+      // raw response
+      // console.log(response);
+   });
+
 });
 
 router.get('/search', (req, res) => {
